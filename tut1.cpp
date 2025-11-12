dfb433009683d427a939c30d3c83c47389441c37
Moulik Rajoria <moulikrajoria001@gmail.com>
2025-11-12 11:50:21 +0530

    more files

---
+++ tut1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    int a,b;
+    a=5;
+    b=4;
+    cout<<"the value of a+b is :"<<a+b<<endl;
+    cout<<"the value of a-b is :"<<a-b<<endl;
+    cout<<"the value of a/b is :"<<a/b<<endl;
+    cout<<"the value of a*b is :"<<a*b<<endl;
+    cout<<"the value of a++ is :"<<a++<<endl;
+    cout<<"the value of a is :"<<a<<endl;
+    cout<<"the value of ++a is :"<<++a<<endl;
+    cout<<"the value of --a is :"<<--a<<endl;
+    cout<<"the value of a-- is :"<<a--<<endl;
+
+
+
+
+    return 0;
+}
